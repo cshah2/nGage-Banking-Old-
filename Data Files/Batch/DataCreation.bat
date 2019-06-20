fc59061269e56a98da5feb71031583971cd1cdba
chintan.shah <chintan.shah@PC860.asl.mum.sst>
2019-06-20 16:18:49 +0530

    Enabled email integration

--- Data Files/Batch/DataCreation.bat
+++ Data Files/Batch/DataCreation.bat
@@ -46,7 +46,7 @@ set EXECUTE_PROFILE=SavEE_SIT
 goto EXECUTE_CMD
 
 :EXECUTE_CMD
-CALL cmd /c start katalon -noSplash  -runMode=console -consoleLog -projectPath="%PRJ_PATH%" -statusDelay=60 -retry=0 -testSuitePath="%TEST_SUITE%" -executionProfile="%EXECUTE_PROFILE%" -browserType="%BROWSER%" -summaryReport
+CALL cmd /c start katalon -noSplash  -runMode=console -consoleLog -projectPath="%PRJ_PATH%" -statusDelay=60 -retry=0 -testSuitePath="%TEST_SUITE%" -executionProfile="%EXECUTE_PROFILE%" -browserType="%BROWSER%" -summaryReport -sendMail="chintan.shah@aurionpro.com"
 goto END
 
 :END
