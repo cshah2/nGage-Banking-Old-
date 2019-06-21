44a60e8c97916e49a7c3522e0dec3fbbbc4fd938
chintan.shah <chintan.shah@PC860.asl.mum.sst>
2019-06-21 14:39:50 +0530

    Updated test cases structure as per Irida's excel sheet.

--- Test Suites/SMOKE.ts
+++ Test Suites/SMOKE.ts
@@ -11,63 +11,63 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>fdafa940-e093-4b54-8b07-7e3faaa880f2</testSuiteGuid>
    <testCaseLink>
-      <guid>19567387-b43d-4c3e-94ce-5dd4bdf043e6</guid>
+      <guid>07ca149c-d3fc-47d8-9924-8d31616faaef</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Smoke/1. (CE_EE) - Login and User management/1. Validate log in (1)</testCaseId>
+      <testCaseId>Test Cases/Smoke/1. (CE_EE) - General/1. (ID - 1.1) Validate log in</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>88402408-ac40-45c3-9a0f-cf3fd2b32ada</guid>
+      <guid>d81bbf2e-c444-4258-8141-2e5b4885a63f</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Smoke/1. (CE_EE) - Login and User management/2. Finxact Version check (2)</testCaseId>
+      <testCaseId>Test Cases/Smoke/1. (CE_EE) - General/2. (ID - 1.2) Finxact Version check</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>31335c9b-9d73-44f9-aaed-d7bfe8516900</guid>
+      <guid>5f9b1264-6733-4466-866c-c20970b152c1</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Smoke/1. (CE_EE) - Login and User management/3. Outsystems tenant check (3)</testCaseId>
+      <testCaseId>Test Cases/Smoke/1. (CE_EE) - General/3. (ID - 1.3) Outsystems tenant check</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>5168d0d3-1e6a-4145-8251-799ced8ff8e4</guid>
+      <guid>be56522b-d5fd-491a-ba0b-b9c5cf912cc0</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Smoke/2. (CE_EE) - Customer and Account Search/1. Customer/Add new customer (6)</testCaseId>
+      <testCaseId>Test Cases/Smoke/2. (CE_EE) - Customer and Account Search/1. Customers/1. (ID - 2) Customer/1. (ID - 2.3) Add new customer</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>84785a3a-80a5-490a-b0bf-024f57a94c27</guid>
+      <guid>14322f0f-1d5e-4d72-9958-8bb2c82c5802</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Smoke/2. (CE_EE) - Customer and Account Search/1. Customer/Add new customer and account (7)</testCaseId>
+      <testCaseId>Test Cases/Smoke/2. (CE_EE) - Customer and Account Search/1. Customers/1. (ID - 2) Customer/2. (ID - 2.4) Add new customer and account</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>c6326c17-6271-4d26-bbcc-237689e868d1</guid>
+      <guid>cb4b875d-0ab5-4c43-9401-84bcc600f128</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Smoke/2. (CE_EE) - Customer and Account Search/2. Account/Validate Customer Accounts populate and account can be added (10)</testCaseId>
+      <testCaseId>Test Cases/Smoke/2. (CE_EE) - Customer and Account Search/1. Customers/1. (ID - 2) Customer/3. (ID - 2.1) Customer Search</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>a51a9086-e378-4f95-abd4-cd81e0d6b1a5</guid>
+      <guid>be128fab-2198-42b7-ad77-4708992a5480</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Smoke/2. (CE_EE) - Customer and Account Search/3. Search/1. Customer Search (4)</testCaseId>
+      <testCaseId>Test Cases/Smoke/2. (CE_EE) - Customer and Account Search/1. Customers/1. (ID - 2) Customer/4. (ID - 2.2) Customer Tabs</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>73947015-d148-437a-90aa-a2cadf89d6ae</guid>
+      <guid>d7a81010-61b9-476d-8ea1-48bd97ffbd50</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Smoke/2. (CE_EE) - Customer and Account Search/3. Search/2. Customer Tabs (5)</testCaseId>
+      <testCaseId>Test Cases/Smoke/2. (CE_EE) - Customer and Account Search/1. Customers/3. (ID - 4) Customer Accounts/1. (ID - 4.1) Validate Customer Accounts populate and account can be added</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>c4ec2aaa-66c7-49a7-89cc-3875c412fb6e</guid>
+      <guid>d8abf19a-aba7-4a9f-98bf-e95d893e7bf0</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Smoke/2. (CE_EE) - Customer and Account Search/3. Search/3. Search for existing account</testCaseId>
+      <testCaseId>Test Cases/Smoke/2. (CE_EE) - Customer and Account Search/2. Accounts/1. (ID - 11) Account/1. (ID - 11.1) Search for existing account</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>f2f34372-26df-4a9b-820a-360f0ed4b27f</guid>
+      <guid>acf7327d-fcf2-4384-9cc3-42a5dedab7d2</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Smoke/2. (CE_EE) - Customer and Account Search/3. Search/4. Validate accounts tab populated</testCaseId>
+      <testCaseId>Test Cases/Smoke/2. (CE_EE) - Customer and Account Search/2. Accounts/1. (ID - 11) Account/2. (ID - 11.2) Validate accounts tab populated</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
