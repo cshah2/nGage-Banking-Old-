17330dc3496f6b6ebc1aa3db30f0447bc454197c
chintan.shah <chintan.shah@PC860.asl.mum.sst>
2019-07-16 13:24:59 +0530

    Updated P1 Automation test suite

--- Test Suites/PRIORITY_1.ts
+++ Test Suites/PRIORITY_1.ts
@@ -11,153 +11,201 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>fdafa940-e093-4b54-8b07-7e3faaa880f2</testSuiteGuid>
    <testCaseLink>
-      <guid>7589d51f-0816-4e4e-9b1e-b4151f4e008a</guid>
+      <guid>871516eb-2b3a-4912-8e01-cfa891b1639d</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Priority 1/1. (CE_EE) - General/1. (ID - 1.1) Validate log in</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>8e9b3cb7-daef-46a1-862d-090972567ebf</guid>
+      <guid>b319cb75-2930-4281-86f2-e73a66e3d2bc</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Priority 1/1. (CE_EE) - General/2. (ID - 1.2) Finxact Version check</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>b81358f1-f120-49d9-92af-a7676efe56f7</guid>
+      <guid>066ebe57-d9d1-4398-b94f-4f9b26f7f9ba</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Priority 1/1. (CE_EE) - General/3. (ID - 1.3) Outsystems tenant check</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>45634c1f-323a-424a-9ccf-60f0427aa63d</guid>
+      <guid>a1808c71-5129-4ddf-8547-cea59c4f2026</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/1. Customers/1. (ID - 2) Customer/1. (ID - 2.3) Add new customer</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>95fba7d2-786f-4f60-a7f0-866649c7d715</guid>
+      <guid>c3f3a438-bf36-454e-9e30-bb4d9d6c63d7</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/1. Customers/1. (ID - 2) Customer/2. (ID - 2.4) Add new customer and account</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>c5b697de-81f7-4dee-939c-5d147017c0b4</guid>
+      <guid>536d2d90-afd2-44b2-8f7b-64d5c0a71670</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/1. Customers/1. (ID - 2) Customer/3. (ID - 2.1) Customer Search</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>340bbfee-edcb-4393-b9eb-82087910a985</guid>
+      <guid>3c1aa2b9-7d85-4b7c-b5ce-3ac1df31d3c5</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/1. Customers/1. (ID - 2) Customer/4. (ID - 2.2) Customer Tabs</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>a4b4fa1d-8ca7-4615-8a67-11ae5e7ebcf3</guid>
+      <guid>f9948676-7af5-4350-85e4-7ee2c4643107</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/1. Customers/2. (ID - 3) Customer Overview/5. (ID - 3.5) Accounts</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>4c393b4f-bd48-4747-b2a8-3acf9c62cd19</guid>
+      <guid>7c6ca66c-02d2-47fd-8115-562e53c00499</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/1. Customers/3. (ID - 4) Customer Accounts/1. (ID - 4.1) Validate Customer Accounts populate and account can be added</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>b71aff85-519c-4e42-941a-212e06eede0d</guid>
+      <guid>cf220ef2-30cc-4227-91c0-455e9d11cc9c</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/1. Customers/5. (ID - 6) Customer Contact Details/1. (ID - 6.1) Add Customer Address</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>71673576-bdcc-4569-8c1e-ad798fae6020</guid>
+      <guid>a2ec02d1-f370-46a4-a5fe-719acd71736f</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/1. Customers/5. (ID - 6) Customer Contact Details/2. (ID - 6.2) Edit Customer Address</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>42b48ff3-4521-4343-afe3-e3ef8b5f822c</guid>
+      <guid>3ac239a3-8651-42f9-afa4-b4499d878d0b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/1. Customers/5. (ID - 6) Customer Contact Details/3. (ID - 6.3) Add Customer Phone</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>50af1d20-16f3-4a8b-823a-ea8deed04990</guid>
+      <guid>1da7f4c2-5ee4-45da-8b38-584a7448608c</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/1. Customers/5. (ID - 6) Customer Contact Details/4. (ID - 6.4) Edit Customer Phone</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>e077788c-3785-4b8b-9d4e-1da964ef271b</guid>
+      <guid>e8bd6d63-b26b-4f84-a965-ac929594f16c</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/1. Customers/5. (ID - 6) Customer Contact Details/5. (ID - 6.5) Add Customer Email</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>ae676c43-4b2e-4ee9-8a9a-81e9a58386fe</guid>
+      <guid>ae8915d5-1159-468a-a9af-005fe58be073</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/1. Customers/5. (ID - 6) Customer Contact Details/6. (ID - 6.6) Edit Customer Email</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>b76ab902-2686-4f4d-8a88-4b9621245d07</guid>
+      <guid>50b340d3-0093-41cc-8c0b-9d48c9ac5d94</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/1. Customers/6. (ID - 7) Customer Notes/1. (ID - 7.1) Add a Customer Note</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>13120109-d0ec-4862-a451-8ae0b6edae85</guid>
+      <guid>30e5fc62-0b84-4d4a-ad82-6400bd3d4816</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/1. Customers/7. (ID - 8) Customer Documents/1. (ID - 8.1) Upload Document</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>202e4c1a-6c19-43ab-8970-43bfc9c39c92</guid>
+      <guid>3b802108-9fcc-456b-80eb-9250ce5e9439</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/1. Customers/7. (ID - 8) Customer Documents/2. (ID - 8.3) Edit Document</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>edb9560f-5ec4-430e-9917-a6e23c065daf</guid>
+      <guid>bbe1299b-c993-4d14-af5c-94cd8cdff306</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/1. Customers/9. (ID - 10) Customer - Task List/2. (ID - 10.3) Add Customer Document</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>c46be49f-f326-430a-9888-dc0646ce1615</guid>
+      <guid>42c82230-396e-40e4-a3e9-9983e35dee42</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/1. Customers/9. (ID - 10) Customer - Task List/3. (ID - 10.4) Add Customer Note</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>9ba6b280-3355-4a03-ac01-0ae6d681e5b5</guid>
+      <guid>ed638956-c3fb-4286-bf21-16b3d0b5038a</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/2. Accounts/1. (ID - 11) Account/1. (ID - 11.1) Search for existing account</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>74c99a67-3879-4454-8697-6168d5371881</guid>
+      <guid>b6a35482-7134-48f1-8453-687b1614e833</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/2. Accounts/1. (ID - 11) Account/2. (ID - 11.2) Validate accounts tab populated</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>2542a61b-57c3-46de-b248-42994a2bdb62</guid>
+      <guid>a7f9e1fd-4881-4275-9079-65669c28a40a</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/2. Accounts/2. Deposit Accounts/1. (ID - 12) DDA Account/1. (ID - 12.1) Add DDA Banking Account</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>c0770b1a-d635-4cc1-bcc0-d082424c5379</guid>
+      <guid>c46ad5d8-1bd0-4468-83da-47fbcf7af66b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/2. Accounts/2. Deposit Accounts/1. (ID - 12) DDA Account/2. (ID - 12.2) Add DDA Transactions - DDA Deposit</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>2b01aef2-2b7e-4cc4-b269-da3cc95def5c</guid>
+      <guid>aff7c408-5e8a-4cbe-8a5e-647bd90e97c3</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/2. Accounts/2. Deposit Accounts/1. (ID - 12) DDA Account/3. (ID - 12.3) Add DDA Transactions - DDA Withdraw</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f8bcc564-7d82-4398-8d23-c18c73ffb0c5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/2. Accounts/2. Deposit Accounts/2. (ID - 13) CD Account/1. (ID - 13.1) Add CD Banking Account</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>08195912-cf7a-4161-9587-c51c71eb8b7d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/2. Accounts/2. Deposit Accounts/2. (ID - 13) CD Account/2. (ID - 13.2) Add CD Transactions - CD Deposit</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7ed7aba1-55a5-4f6f-86d3-015846750176</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/2. Accounts/2. Deposit Accounts/2. (ID - 13) CD Account/3. (ID - 12.3) Add CD Transactions - CD Withdraw</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>01aac14e-269e-4fe0-bf55-ce30545e9efc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/2. Accounts/2. Deposit Accounts/3. (ID - 14) Savings Account/1. (ID - 14.1) Add Savings Account</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9a8e5545-9ba8-4dc3-b899-8f9d9997421e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/2. Accounts/2. Deposit Accounts/3. (ID - 14) Savings Account/2. (ID - 14.2) Add Savings Transactions - Savings Miscellaneous Credit</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1fac37f3-35a8-4200-af36-6226fdffe6ca</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/2. Accounts/2. Deposit Accounts/3. (ID - 14) Savings Account/3. (ID - 14.3) Add Savings Transactions - Savings Miscellaneous Debit</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8edcfc99-c86c-4a7d-b766-bab1b6190817</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/2. Accounts/2. Deposit Accounts/3. (ID - 14) Savings Account/4. (ID - 14.4) Add Savings Transactions - Savings Deposit Cash</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c68986c3-af25-49ba-abc0-dd67c1cd49eb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/2. Accounts/2. Deposit Accounts/4. (ID - 15) Deposit Account - Details/1. (ID - 15.3) - Add Documents</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
