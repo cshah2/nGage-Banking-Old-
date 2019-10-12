243d8034572932e8b644f9b6e65c0466577411fe
Chintan <Chintan@Chintan-PC>
2019-10-12 19:25:39 +0530

    Updated test suite

--- Test Suites/TMP_SUITE.ts
+++ Test Suites/TMP_SUITE.ts
@@ -11,15 +11,57 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>1a2184d7-23b8-48bb-9598-e3e53dabb042</testSuiteGuid>
    <testCaseLink>
-      <guid>bb4b9c0f-ebd8-42cb-820e-18c396a98abe</guid>
+      <guid>12e27326-be91-40a3-b829-7b168f8bf59b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/3. Organizations/A. (ID - 100) Organization/1. (ID - 100.3) Add new organization</testCaseId>
+      <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/1. Customers/A. (ID - 2) Customer/1. (ID - 2.3) Add new customer</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>727c1456-18aa-4dd4-aed5-8c3ee205753d</guid>
+      <guid>db2cb7ce-2af1-410d-b980-b24606f50c35</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/3. Organizations/A. (ID - 100) Organization/3. (ID - 100.1) Organization Search</testCaseId>
+      <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/2. Accounts/2. Deposit Accounts/6. (ID - 20) Deposit Accounts Orders/A. (ID - 20.1) Add Orders - Book Transfer</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f5b2ca9a-60c8-4ca4-8c2f-2eb9aeba88a2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/2. Accounts/2. Deposit Accounts/6. (ID - 20) Deposit Accounts Orders/B. (ID - 20.2) Add Orders - Payment Order</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ba4720f3-3523-4d65-b183-3c630736b6f6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/2. Accounts/2. Deposit Accounts/6. (ID - 20) Deposit Accounts Orders/C. (ID - 20.3) Add Orders - Collection Order</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>40610ac7-0fcd-4e06-a72a-a7633d1a0d83</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/2. Accounts/2. Deposit Accounts/6. (ID - 20) Deposit Accounts Orders/D. (ID - 20.4) Add future-dated order - Book Transfer</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b0eaee2f-2ec9-49a7-9b19-b4437f0a88ee</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/2. Accounts/2. Deposit Accounts/6. (ID - 20) Deposit Accounts Orders/E. (ID - 20.5) Add future-dated order - Payment Order</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e3e24819-1360-4e03-806f-c2839d204b74</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/2. Accounts/2. Deposit Accounts/6. (ID - 20) Deposit Accounts Orders/F. (ID - 20.6) Add future-dated order - Collection Order</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>98045978-321b-4489-8fa9-9c8ab52d60e3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/2. Accounts/2. Deposit Accounts/6. (ID - 20) Deposit Accounts Orders/G. (ID - 20.7) Edit Order</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>89eb9ce7-0988-424b-bf1d-2a4966c67711</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Priority 1/2. (CE_EE) - Customer and Account Search/2. Accounts/2. Deposit Accounts/6. (ID - 20) Deposit Accounts Orders/H. (ID - 20.8) Cancel Order</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
